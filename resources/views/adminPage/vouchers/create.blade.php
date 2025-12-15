919ba0870290952ec7f6e5418b9707289df2afd1
halcyonoidd <agriazzukhruf1@gmail.com>
2025-12-15 08:44:52 +0700

    responsif admin

--- resources/views/adminPage/vouchers/create.blade.php
+++ resources/views/adminPage/vouchers/create.blade.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/adminPage/dashboard.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/adminPage/admin-responsive.css') }}">
 </head>
 <body>
     <div class="dashboard-container">
--- resources/views/guestPage/landingGuest.blade.php
+++ resources/views/guestPage/landingGuest.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="{{ asset('css/vendors/bulma.min.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/frontend/landing.css') }}">
-
+    <link rel="stylesheet" href="{{ asset('css/frontend/mobile-responsive.css') }}">
 </head>
 <body>
     <nav class="navbar transparent" id="navbar">
